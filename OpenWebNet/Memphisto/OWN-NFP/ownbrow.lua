3af28cc9b4c3ce16fab2656b106f9b5e7edbe566
Vladislav <wladeus@drittereich.de>
2021-02-13 12:01:41 +0100

    Big update

--- OpenWebNet/Memphisto/OWN-NFP/ownbrow.lua
+++ OpenWebNet/Memphisto/OWN-NFP/ownbrow.lua
@@ -1,52 +1,34 @@
-_VER_ = "0.08c"
+_VER_ = "0.04a"
 
+local computer = require("computer")
 local component = require("component")
 local tty = require("tty")
 local seriz = require("serialization")
 local fs = require("filesystem")
 local ev = require("event")
 local unicode = require("unicode")
+local render = require("doubleBuffering")
 
 local vid = component.gpu
 
+local page, why, lab, conf, servs
+local line = false
+local ocifs = {}
+local refs = {}
+local dwrefs = {}
+local padd, lpadd
+local picrnd = true
+local rx = 0
+local ry = 0
+local hoh = true
 
-local function globalnil()
-	line = nil
-	lfil = nil
-	ocifs = nil
-	refs = nil
-	dwrefs = nil
-	padd = nil
-	lpadd = nil
-	picrnd = nil
-	conf = nil
-	page = nil
-	rx = nil
-	ry = nil
-	why = nil
-	hoh = nil
-end
-
-line = false
-lfil = true
-ocifs = {}
-refs = {}
-dwrefs = {}
-padd = ''
-lpadd = ''
-picrnd = true
-conf = {}
-page = {}
-rx = 0
-ry = 0
-hoh = true
+local mx, my = vid.getResolution()
 	
 print("Memphisto OWN-NFP v".._VER_)
-print("Developing © 2020 Compys S&N Systems")
+print("Developing © 2020-2021 Compys S&N Systems")
 print("Loading OCIF library...")
 
 local piclib = require("image")
-local color = require("color")
 
 local function prac()
 	print("Press any key to continue.")
@@ -65,7 +47,7 @@ local function split(inp, sep)
 end
 
 local function restore(table)
-        res = ''
+        local res = ''
         for _, val in pairs(table) do
         	res = res..val..'/'
         end
@@ -86,12 +68,12 @@ local function goerr(mode)
 	local rawpage, why = io.open(opn)
 	if rawpage == nil then
 		tty.clear()
-		globalnil()
+		
 		print("FATAL ERROR! Can't open system page file: "..why) os.exit() end
-	page, why = seriz.unserialize(rawpage:read())
+	page, why = seriz.unserialize(rawpage:read(fs.size(opn)))
 	if page == nil then
 		tty.clear()
-		globalnil()
+		
 		print("FATAL ERROR! Can't handle system page file: "..why) os.exit() end
 	lab = page['label']
 	rx = 0
@@ -100,132 +82,137 @@ local function goerr(mode)
 end 
 
 local function drawocif(kx, ky, ptox, locl)
+	local _, mess, mess2
 	if padd ~= lpadd then ocifs = {} lpadd = padd end
-	parts = split(ptox, "/")
+	local parts = split(ptox, "/")
 	if parts[1] == ".." then
-		parts2 = split(padd, "/")
+		local parts2 = split(padd, "/")
 		parts[1]= parts2[1]
 		ptox = restore(parts)
 	end
 	if picrnd == false then
-			vid.setBackground(conf.std_backg)
-			vid.setForeground(0xFF0000)
-			vid.set(kx, ky, "PICTURE: "..ptox)
+			render.drawText(kx, ky, 
+				 0xFF0000, "PICTURE: "..ptox)
 			return
 	end
 	if locl == false then
 		if ocifs[ptox] ~= nil then
-			piclib.draw(kx,ky,ocifs[ptox])
+			render.drawImage(kx, ky, ocifs[ptox])
 		else
-			parts = split(ptox, "/")
-			opp = servs[parts[1]:lower()]
+			local opp = servs[parts[1]:lower()]
 			if opp == nil then
-				vid.setBackground(conf.std_backg)
-				vid.setForeground(0xFF0000)
-				vid.set(kx, ky, "ERROR: No server found for picture")
+				render.drawText(kx, ky, 
+				 0xFF0000, "ERROR: No server found for picture")
 				return
 			end
 			parts[1] = nil
-			ratd = restore(parts)
+			local ratd = restore(parts)
 			card.send(opp, 3707, "GIM", ratd)
 			_, _, _, _, _, mess, mess2 = ev.pull(5, "modem_message")
 			if mess == nil then
-				vid.setBackground(conf.std_backg)
-				vid.setForeground(0xFF0000)
-				vid.set(kx, ky, "ERROR: No answer from server")
+				render.drawText(kx, ky, 
+				 0xFF0000, "ERROR: No answer from server")
 				return
 			elseif mess == 'FOK' then
-				rpage = ''
+				local rpage = ''
 				while mess ~= "FEND" do
 					_, _, _, _, _, mess, mess2 = ev.pull(5, "modem_message")
 					if mess == "FTR" then
 						rpage = rpage..mess2
 					end
 				end
-				pic = piclib.fromString(rpage)
+				local pic = piclib.fromString(rpage)
 				if ocifs[ptox] == nil then ocifs[ptox] = pic end
-				piclib.draw(kx,ky,pic)
+				render.drawImage(kx, ky, pic)
 			elseif mess == '404' then
-				vid.setBackground(conf.std_backg)
-				vid.setForeground(0xFF0000)
-				vid.set(kx, ky, "ERROR: Picture no found on server")
+				render.drawText(kx, ky, 
+				 0xFF0000, "ERROR: Picture not found on server")
 				return
 			end
 		end
 	else
 		if ocifs[ptox] ~= nil then
-			piclib.draw(kx,ky,ocifs[ptox])
+			render.drawImage(kx, ky, ocifs[ptox])
 			return
 		end
-		pic, why = piclib.load(ptox)
+		local pic, why = piclib.load(ptox)
 		if pic == false then
-			vid.setBackground(conf.std_backg)
-			vid.setForeground(0xFF0000)
 			if why ~= nil then
-				vid.set(kx, ky, "ERROR: Can't load picture: "..why)
+				render.drawText(kx, ky, 
+				 0xFF0000, "ERROR: Can't load picture: "..why)
 			else
-				vid.set(kx, ky, "ERROR: Can't load picture: unknown reason")
+				render.drawText(kx, ky, 
+				 0xFF0000, "ERROR: Can't load picture: unknown reason")
 			end
 			return
 		end
 		if ocifs[ptox] == nil then ocifs[ptox] = pic end
-		piclib.draw(kx,ky,pic)
+		render.drawImage(kx, ky, pic)
 	end
 end
 
 local function drawpage(rx, ry, sbk)
 	refs = {}
 	dwrefs = {}
-	pos = 0
-	mx, my = vid.getResolution()
-	vid.setBackground(sbk)
-  	vid.setForeground(0xFFFFFF)
-  	vid.fill(1,1,mx,my, " ")
+	local pos = 0
+	render.clear(sbk)
 	while pos ~= #page do
 		pos= pos+1
 		if page[pos][1] == 0 then
 			if conf.autobackg == true and page[pos][5] == 0x000000 then
-				vid.setBackground(sbk)
+				render.drawText(page[pos][2]+rx, page[pos][3]+ry, 
+				 page[pos][4], page[pos][6])
 			else
-				vid.setBackground(page[pos][5])
+				
+				render.drawRectangle(page[pos][2]+rx, page[pos][3]+ry,
+				 unicode.len(page[pos][6]), 1, page[pos][5], page[pos][4], ' ')
+				render.drawText(page[pos][2]+rx, page[pos][3]+ry, 
+				 page[pos][4], page[pos][6])
 			end
-			vid.setForeground(page[pos][4])
-			vid.set(page[pos][2]+rx, page[pos][3]+ry, page[pos][6])
 		elseif page[pos][1] == 1 then
 			if conf.autobackg == true and page[pos][5] == 0x000000 then
-				vid.setBackground(sbk)
+				render.drawText(page[pos][2]+rx, page[pos][3]+ry, 
+				 page[pos][5], page[pos][7])
 			else
-				vid.setBackground(page[pos][6])
+				render.drawRectangle(page[pos][2]+rx, page[pos][3]+ry,
+				 unicode.len(page[pos][7]), 1, page[pos][6], page[pos][7], ' ')
+				render.drawText(page[pos][2]+rx, page[pos][3]+ry, 
+				 page[pos][5], page[pos][7])
 			end
-			vid.setForeground(page[pos][5])
-			vid.set(page[pos][2]+rx, page[pos][3]+ry, page[pos][7])
 			table.insert(refs, {page[pos][2]+rx, page[pos][2]+rx+#page[pos][7],
 			page[pos][3]+ry, page[pos][4], page[pos][8]})
 		elseif page[pos][1] == 2 then
 			if conf.autobackg == true and page[pos][5] == 0x000000 then
-				vid.setBackground(sbk)
+				render.drawText(page[pos][2]+rx, page[pos][3]+ry, 
+				 page[pos][5], page[pos][7])
 			else
-				vid.setBackground(page[pos][6])
+				render.drawRectangle(page[pos][2]+rx, page[pos][3]+ry,
+				 unicode.len(page[pos][7]), 1, page[pos][6], page[pos][7], ' ')
+				render.drawText(page[pos][2]+rx, page[pos][3]+ry, 
+				 page[pos][5], page[pos][7])
 			end
-			vid.setForeground(page[pos][5])
-			vid.set(page[pos][2]+rx, page[pos][3]+ry, page[pos][7])
 			table.insert(dwrefs, {page[pos][2]+rx, page[pos][2]+rx+#page[pos][7],
 			page[pos][3]+ry, page[pos][4], page[pos][8]})
 		elseif page[pos][1] == 3 then
 			drawocif(page[pos][2]+rx, page[pos][3]+ry, page[pos][5] ,page[pos][4])
 		end
 	end
+	if conf.showram then
+		local free = computer.freeMemory()
+		render.drawRectangle(mx-#tostring(free), 1, #tostring(free), 1, 0xFFFFFF, 0x000000, ' ')
+		render.drawText(mx-#tostring(free), 1, 0x000000, free)
+	end
+	render.drawChanges()
 end
 
-local function drawplate(pnam, ofon, lf, lab)
-	mx, my = vid.getResolution()
+local function drawplate(pnam, ofon, lab)
 	vid.setForeground(0x000000)
   	vid.setBackground(0xFFFFFF)
   	vid.fill(1,my,mx,1, " ")
   	vid.set(2, my, "Memphisto OWN-NFP v".._VER_)
   	tform = ''
   	if lab ~= nil and lab ~= '' then tform = ' - '..lab end
-  	if lf == false then
+  	if hoh == false then
   		vid.set(36, my, "Page: "..pnam..tform)
   	else
   		vid.set(36, my, "Page: "..lab)
@@ -243,7 +230,9 @@ end
 
 local function setline()
 	tty.clear()
-	drawplate(padd, line, lfil, lab)
+	render.clear(0)
+	render.drawChanges()
+	drawplate(padd, line, lab)
 	if line == false then
 		if component.list("modem")() == nil then 
 		print("No Network Card is detected.\nYou can't use ON-LINE mode!") prac() return end
@@ -257,7 +246,7 @@ local function setline()
 			if servs == nil then
 				print("CRITICAL ERROR! Can't handle local DDBServer file!") prac() return end
 			line = true
-				print("OK! Now you are ON-LINE") drawplate(padd, line, lfil, lab) os.sleep(1,5)	
+				print("OK! Now you are ON-LINE") drawplate(padd, line, lab) os.sleep(1,5)	
 		else
 			print("Connecting to DDBServer on "..conf["DDBS_serv"])
 			card.send(conf["DDBS_serv"], 3707, 'GADL')
@@ -269,16 +258,18 @@ local function setline()
 				if servs == nil then
 					print("CRITICAL ERROR! Can't handle remote DDBServer file!") prac() return end
 				line = true
-				print("OK! Now you are ON-LINE") drawplate(padd, line, lfil, lab) os.sleep(1,5)
+				print("OK! Now you are ON-LINE") drawplate(padd, line, lab) os.sleep(1,5)
 			end
 		end
 	else
 		line = false
-		print("OK! Now you are OFF-LINE") drawplate(padd, line, lfil, lab) os.sleep(1,5)
+		print("OK! Now you are OFF-LINE") drawplate(padd, line, lab) os.sleep(1,5)
 	end
+	
 end
 
 local function openpage(inp, loca)
+	local _, mess, mess2
 	if inp == "" then return page end
 	if loca == nil then loca = not line end
 	parts = split(inp, "/")
@@ -289,12 +280,11 @@ local function openpage(inp, loca)
 	end
 	rx = 0
 	ry = 0
-	hoh = false
-	lfil = true
-	vid.setBackground(0x000000)
-  	vid.setForeground(0xFFFFFF)
 	tty.clear()
-	drawplate(padd, line, lfil, lab)
+	render.clear(0)
+	render.drawChanges()
+	drawplate(padd, line, lab)
+	hoh = false
 	print("Opening page: "..inp)
 	if loca == false then
 		padd = inp
@@ -306,7 +296,7 @@ local function openpage(inp, loca)
 		end
 		print("Server address found in DB, connecting...")
 		parts[1] = nil
-		ratd = restore(parts)
+		local ratd = restore(parts)
 		card.send(opp, 3707, "GPG", ratd)
 		_, _, _, _, _, mess, mess2 = ev.pull(5, "modem_message")
 		if mess == nil then
@@ -314,7 +304,7 @@ local function openpage(inp, loca)
 			return
 		elseif mess == 'FOK' then
 			print("Connected, getting page...")
-			rpage = ''
+			local rpage = ''
 			while mess ~= "FEND" do
 				_, _, _, _, _, mess, mess2 = ev.pull(5, "modem_message")
 				if mess == "FTR" then
@@ -345,7 +335,6 @@ local function openpage(inp, loca)
 			goerr(3)
 			return end
 		if page['label'] ~= nil then lab = page['label'] else lab = '' end
-		lfil = false
 		return 
 	else
 		padd = inp
@@ -354,13 +343,12 @@ local function openpage(inp, loca)
 			goerr(3)
 			return
 		end
-		page = seriz.unserialize(rawpage:read())
+		page = seriz.unserialize(rawpage:read(fs.size(padd)))
 		if page == nil then
 			goerr(0)
 			return
 		end
 		if page['label'] ~= nil then lab = page['label'] else lab = '' end
-		lfil = fals
 		rawpage:close()
 		return
 	end
@@ -382,7 +370,6 @@ local function enterurl()
     				inp = inp:sub(1,pos-1)
     				vid.set(pos+42,my, ' ')
   			elseif key == 13 then
-  				lfil = true
     				openpage(inp, not line)
     				return
     			elseif key == 0 or key == 9 or key == 127 then
@@ -405,15 +392,12 @@ local function gohome()
 	local rawpage, why = io.open(padd)
 	if rawpage == nil then
 		tty.clear()
-		globalnil()
 		print("FATAL ERROR! Can't open homepage file: "..why) os.exit() end
-	page, why = seriz.unserialize(rawpage:read())
+	page, why = seriz.unserialize(rawpage:read(fs.size(padd)))
 	if page == nil then
 		tty.clear()
-		globalnil()
 		print("FATAL ERROR! Can't handle homepage file: "..why) os.exit() end
 	if page['label'] ~= nil then lab = page['label'] else lab = '' end
-	lfil = true
 	rawpage:close()
 	rx = 0
 	ry = 0
@@ -425,15 +409,12 @@ local function helpmepls()
 	local rawpage, why = io.open(padd)
 	if rawpage == nil then
 		tty.clear()
-		globalnil()
 		print("FATAL ERROR! Can't open system page file: "..why) os.exit() end
-	page, why = seriz.unserialize(rawpage:read())
+	page, why = seriz.unserialize(rawpage:read(fs.size(padd)))
 	if page == nil then
 		tty.clear()
-		globalnil()
 		print("FATAL ERROR! Can't handle system page file: "..why) os.exit() end
 	if page['label'] ~= nil then lab = page['label'] else lab = '' end
-	lfil = true
 	rawpage:close()
 	rx = 0
 	ry = 0
@@ -454,19 +435,22 @@ local function paradraw()
 		ry = 0
 	end
 	drawpage(rx, ry, conf["std_backg"])
-	drawplate(padd, line, lfil, lab)
+	drawplate(padd, line, lab)
 end
 
 local function download(loca, dwp)
+	local _, mess, mess2, key
 	if loca == nil then loca = not line end
 	tty.clear()
-	drawplate(padd, line, lfil, lab)
+	render.clear(0)
+	render.drawChanges()
+	drawplate(padd, line, lab)
 	if loca == false then
-		parts = split(dwp, "/")
+		local parts = split(dwp, "/")
 		if parts[1] == ".." then
-			parts2 = split(padd, "/")
+			local parts2 = split(padd, "/")
 			parts[1]= parts2[1]
-			dwp = restore(parts)
+			local dwp = restore(parts)
 		end
 		print('Do you want to download file: '..parts[#parts]..'? Y/N')
 		while true do
@@ -474,14 +458,14 @@ local function download(loca, dwp)
 			if key == 21 then break
 			elseif key == 49 then paradraw() return end
 		end
-		opp = servs[parts[1]:lower()]
+		local opp = servs[parts[1]:lower()]
 		if opp == nil then
 			print("ERROR: No server found for file")
 			prac()
 			return
 		end
 		parts[1] = nil
-		ratd = restore(parts)
+		local ratd = restore(parts)
 		print("Downloading file...")
 		card.send(opp, 3707, "GFL", ratd)
 		_, _, _, _, _, mess, mess2 = ev.pull(5, "modem_message")
@@ -490,8 +474,8 @@ local function download(loca, dwp)
 			prac()
 			return
 		elseif mess == 'FOK' then
-			savto = fs.concat(conf.download_dir, parts[#parts])
-			sav = io.open(savto, 'wb')
+			local savto = fs.concat(conf.download_dir, parts[#parts])
+			local sav = io.open(savto, 'wb')
 			while mess ~= "FEND" do
 				_, _, _, _, _, mess, mess2 = ev.pull(5, "modem_message")
 				if mess == "FTR" then
@@ -508,8 +492,8 @@ local function download(loca, dwp)
 			return 
 			end
 	else
-		fil = io.open(dwp, 'rb')
-		parts = split(dwp, "/")
+		local fil = io.open(dwp, 'rb')
+		local parts = split(dwp, "/")
 		print('Do you want to download file: '..parts[#parts]..'? Y/N')
 		while true do
 			_, _, _, key = ev.pull("key_up")
@@ -517,9 +501,9 @@ local function download(loca, dwp)
 			elseif key == 49 then return end
 		end
 		print("Downloading file...")
-		savto = fs.concat(conf.download_dir, parts[#parts])
-		sav = io.open(savto, 'wb')
-		rded = fil:read()
+		local savto = fs.concat(conf.download_dir, parts[#parts])
+		local sav = io.open(savto, 'wb')
+		local rded = fil:read()
 		while rded ~= nil do
 			sav:write(rded)
 			rded = fil:read()
@@ -541,7 +525,7 @@ local function clickop(kx, ky)
 				rx=0
 				ry=0
 				drawpage(rx, ry, conf["std_backg"])
-				drawplate(padd, line, lfil, lab)
+				drawplate(padd, line, lab)
 			end
 		end
 		for _, dref in pairs(dwrefs) do
@@ -570,15 +554,16 @@ padd = conf["homepage"]
 local rawpage, why = io.open(conf["homepage"])
 if rawpage == nil then
 	print("FATAL ERROR! Can't open homepage file: "..why) return end
-page, why = seriz.unserialize(rawpage:read())
+page, why = seriz.unserialize(rawpage:read(fs.size(conf["homepage"])))
 if page == nil then
 	print("FATAL ERROR! Can't handle homepage file: "..why) return end
 if page['label'] ~= nil then lab = page['label'] else lab = '' end
 rawpage:close()
 
-
+render.clear(0)
+render.drawChanges()
 drawpage(rx, ry, conf["std_backg"])
-drawplate(conf["homepage"], line, lfil, lab)
+drawplate(conf["homepage"], line, lab)
 
 while true do
 	local eve,_,x,key,sc = ev.pull()
@@ -597,8 +582,6 @@ while true do
 		if line == true then card.close(3707) end
 		rawpage:close()
 		tty.clear()
-		globalnil()
-		if line == true then card.close(3707) end
 		print("Thanks for using Memphisto!\n")
 		os.exit()
 	elseif eve == "scroll" then
